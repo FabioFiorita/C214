11fd3b9ba9e9dac6095c2e17776d917d1edf2b7e
Fabio Fiorita <fabiolfp@gmail.com>
2022-08-23 15:02:55 -0300

    Creating AtendimentoService interface

---
+++ ExercicioPratico1/src/main/java/br/inatel/cdg/Atendimento.java
@@ -0,0 +1,16 @@
+package br.inatel.cdg;
+
+public class Atendimento {
+
+    private String nomeProfessor;
+    private String horarioAtendimento;
+    private String periodo;
+
+    public Atendimento(String nomeProfessor, String horarioAtendimento, String periodo) {
+        this.nomeProfessor = nomeProfessor;
+        this.horarioAtendimento = horarioAtendimento;
+        this.periodo = periodo;
+    }
+
+
+}
